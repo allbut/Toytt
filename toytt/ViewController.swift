83d84eb23ae34ca6575e28656b0a2389b5a9f451
user <user@alusuirdemacbook-pro.local>
2018-07-17 19:26:01 +0800

    Initial Commit

---
+++ toytt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toytt
+//
+//  Created by user on 2018/7/17.
+//  Copyright © 2018年 grayworm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
